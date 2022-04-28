35858abe295d422398e4b5a2d6cd820d59d1751b
kenfelix <kmrapper.kf@gmail.com>
2022-04-28 20:39:25 +0000

    new

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,35 @@ Module contains a Cache class
 """
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Optional, Callable
+from functools import wraps
+
+
+def call_history(method: Callable) -> Callable:
+    """ memorize user actions"""
+    method_key = method.__qualname__
+    inputs = method_key + ':inputs'
+    outputs = method_key + ':outputs'
+
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """ function  wrapped """
+        self._redis.rpush(inputs, str(args))
+        data = method(self, *args, **kwds)
+        self._redis.rpush(outputs, str(data))
+        return data
+    return wrapper
+
+def count_calls(method: Callable) -> Callable:
+    """ COunts how many times the cache methods are called"""
+    method_key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """wrapped function"""
+        self._redis.incr(method_key)
+        return method(self, *args, **kwds)
+    return wrapper
 
 class Cache:
     """A Cache class"""
@@ -14,7 +42,24 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
+        """stores key value in redis server"""
         randKey = str(uuid4())
         self._redis.mset({randKey: data})
         return randKey
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """ get the element and return the decoded version"""
+        data = self._redis.get(key)
+        if (fn is not None):
+            return fn(data)
+        return data
+
+    def get_str(self, data: str) -> str:
+        """parametrize Cache.get with the correct conversion function"""
+        return data.decode('utf-8')
+
+    def get_int(self, data: str) -> int:
+        """parametrize Cache.get with the correct conversion function"""
+        return int(data)
